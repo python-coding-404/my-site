ad753da2061ac63192cd3e1a93ca9ea8bcac5cc7
root <root@DESKTOP-B3UJH6T.localdomain>
2021-12-14 18:27:51 +0400

    lol

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express")
 const app = express();
 const body = require("body-parser")
-app.use(body.json())
-
+app.use(body.urlencoded())
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/static"))
 
 app.get("/", (req,res) => {
-	res.redirect("/home") 
+        res.cookie("o","o")
+	res.redirect(`/home?lol=${req.query.lol}`) 
 })
 app.get('/home', (req,res) => {
 	res.render("lol",{lol:req.query.lol, lola:"lol"});
 })
+app.use((req,res,next) => {
+	res.render("404")
+	next()
+})
 app.listen(8080);
+
 
